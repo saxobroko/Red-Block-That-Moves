1185ae73180444e1a6dfb21711c66c2348e4bd87
Saxo_Broko <rascalxena@y7mail.com>
2022-01-12 22:26:04 +1100

    Discord intergration

---
+++ Assets/Scripts/Discordc.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Discord;
+
+public class Discordc : MonoBehaviour {
+
+	public Discord.Discord discord;
+
+	// Use this for initialization
+	void Start () {
+		discord = new Discord.Discord(930771027850588180, (System.UInt64)Discord.CreateFlags.NoRequireDiscord);
+		var activityManager = discord.GetActivityManager();
+		var activity = new Discord.Activity
+		{
+			State = "and stuff...",
+			Details = "Running"
+		};
+		activityManager.UpdateActivity(activity, (res) =>
+		{
+			if (res == Discord.Result.Ok)
+			{
+				Debug.LogError("Everything is fine!");
+			}
+		});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		discord.RunCallbacks();
+	}
+}
